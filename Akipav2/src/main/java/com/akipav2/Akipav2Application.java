9b8fd32546eae34116effc55c94df9a81b059940
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-02-02 12:10:35 -0500

    Desplegable del proyecto

--- Akipav2/src/main/java/com/akipav2/Akipav2Application.java
+++ Akipav2/src/main/java/com/akipav2/Akipav2Application.java
@@ -2,12 +2,18 @@ package com.akipav2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Akipav2Application {
+public class Akipav2Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Akipav2Application.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Akipav2Application.class);
+	}
 }
